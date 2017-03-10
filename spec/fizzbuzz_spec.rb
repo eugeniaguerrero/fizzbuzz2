bf04d5ccb57015a354d9c50fb56fdc843fa4e56f
Euge <angeleuge95@gmail.com>
2017-03-10 18:56:51 +0000

    adds a new way to call fizzbuzz

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,25 +1,23 @@
-require './lib/fizzbuzz'
+require 'fizzbuzz'
 
-describe Fixnum do
+describe Integer do
   it 'returns "fizzbuzz" for the number 15' do
     expect(15.fizzbuzz).to eq 'fizzbuzz'
   end
-end
 
-describe Fixnum do
+
   it 'returns "fizz" for the number 3' do
     expect(3.fizzbuzz).to eq 'fizz'
   end
-end
 
-describe Fixnum do
-  it 'returns "fizz" for the number 5' do
+
+  it 'returns "buzz" for the number 5' do
     expect(5.fizzbuzz).to eq 'buzz'
   end
-end
 
-describe Fixnum do
+
   it 'returns 1 for the number 1' do
     expect(1.fizzbuzz).to eq 1
   end
+
 end
